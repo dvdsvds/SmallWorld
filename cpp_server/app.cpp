897c84fa280bd3bf19576eba723731140f0ad95c
dvdsvds <c130vy@gmail.com>
2025-02-16 06:44:57 +0000

    done connect to client

--- cpp_server/app.cpp
+++ cpp_server/app.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <curl/curl.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 8080
 
 size_t wcb(void *contents, size_t size, size_t nmemb, std::string *output) {
     size_t ts = size * nmemb;
@@ -9,11 +16,18 @@ size_t wcb(void *contents, size_t size, size_t nmemb, std::string *output) {
 
 }
 
-bool slreq(const std::string& username, const std::string& password) {
+void logError(const std::string& err) {
+    std::cerr << "[ERROR] " << err << std::endl; 
+}
+
+void logInfo(const std::string& info) {
+    std::cout << "[INFO] " << info << std::endl; 
+}
+
+bool slreq(const std::string& username, const std::string& password, std::string& response) {
     CURL *curl;
     CURLcode res;
-
-    std::string url = "http://127.0.0.1:5000/login";
+    std::string url = "http://192.168.50.246:5000/login";
     std::string jd = "{\"username\":\"" + username + "\", \"password\":\"" + password + "\"}";
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
@@ -26,7 +40,6 @@ bool slreq(const std::string& username, const std::string& password) {
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jd.c_str());
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, h);
 
-        std::string response;
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, wcb);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
@@ -52,7 +65,7 @@ bool srreq(const std::string& username, const std::string& password) {
     CURL *curl;
     CURLcode res;
 
-    std::string url = "http://127.0.0.1:5000/register";
+    std::string url = "http://192.168.50.246:5000/register";
     std::string jd = "{\"username\":\"" + username + "\", \"password\":\"" + password + "\"}";
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
@@ -88,37 +101,67 @@ bool srreq(const std::string& username, const std::string& password) {
 
 }
 
-int main() {
-    while(true) {
-        std::string command;
-        std::string username, password;
-        std::cout << "What do you want to do?" << std::endl;
-        std::cout << "]> ";
-        std::getline(std::cin, command);
-
-        if(command == "l") {
-            std::cout << "Username > ";
-            std::getline(std::cin, username);
-
-            std::cout << "Password > ";
-            std::getline(std::cin, password);
-
-            std::cout << "로그인 요청 : ";
-            slreq(username, password);
-        }
-        else if(command == "r") {
-            std::cout << "Username > ";
-            std::getline(std::cin, username);
-
-            std::cout << "Password > ";
-            std::getline(std::cin, password);
-
-            std::cout << "회원가입 요청 : ";
-            srreq(username, password);
-        }
-
-
-
+void startSocket() {
+    int sSocket, cSocket;
+    struct sockaddr_in sAddr, cAddr;
+    socklen_t addrSize = sizeof(cAddr);
+    
+    sSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(sSocket < 0) {
+        logError("Socket 생성 실패");
+        return;
     }
+
+    memset(&sAddr, 0, sizeof(sAddr));
+    sAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, "192.168.50.246", &sAddr.sin_addr);
+    sAddr.sin_port = htons(PORT);
+
+    if(bind(sSocket, (struct sockaddr*)&sAddr, sizeof(sAddr)) < 0) {
+        logError("바인드 실패");
+        return;
+    }
+
+
+    if(listen(sSocket, 5) < 0) {
+        logError("연결 대기 실패");
+        return;
+    }
+
+    logInfo("c++ 소켓이 192.168.50.246:" + std::to_string(PORT) + "에서 실행 중");
+
+    while(true) {
+        cSocket = accept(sSocket, (struct sockaddr*)&cAddr, &addrSize);
+        if(cSocket < 0) {
+            logError("수락 실패");
+            continue;
+        }
+
+        char buffer[1024] = {0};
+        recv(cSocket, buffer, 1024, 0);
+        std::string req(buffer);
+        logInfo("[LOGIN] " + req);
+
+        std::string command, username, password;
+        std::istringstream iss(req);
+        iss >> command >> username >> password;
+
+        std::string flaskRes;
+        if(command == "LOGIN") {
+            slreq(username, password, flaskRes);
+        }
+        else if(command == "REGISTER") {
+            // todo
+        }
+
+        send(cSocket, flaskRes.c_str(), flaskRes.size(), 0);
+        close(cSocket);
+    }
+    close(sSocket);
+}
+
+
+int main() {
+    startSocket();
     return 0;
 }
--- python_server/app.py
+++ python_server/app.py
@@ -16,15 +16,15 @@ def login():
 
     # 사용자 정보가 없을 경우 처리
     if user is None or len(user) == 0:
-        return jsonify({"status": "error", "message": "사용자가 존재하지 않습니다."}), 404
+        return jsonify({"status": "error", "message": "UDNE"}), 404
 
     stored_pwd = user[0][2]  # 비밀번호는 3번째 인덱스 (id, username, password)
     
     # 비밀번호 확인
     if bcrypt.checkpw(password.encode('utf-8'), stored_pwd.encode('utf-8')):
-        return jsonify({"status": "success", "message": "로그인 성공"}), 200
+        return jsonify({"status": "success", "message": "LS"}), 200
     else:
-        return jsonify({"status": "error", "message": "비밀번호가 틀립니다."}), 401
+        return jsonify({"status": "error", "message": "IP"}), 401
 
 # 회원가입 API
 @app.route('/register', methods=['POST'])
